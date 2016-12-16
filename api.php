522b23ca86c16b25f959d1e02f1e28429df16cf9
Afif Zafri <zafri_21596@yahoo.com>
2016-12-16 13:01:37 +0800

    switch to cURL, and receive tracking no with GET

--- api.php
+++ api.php
@@ -1,35 +1,41 @@
 <?php
 
-//http://poslaju.com.my/track-trace-v2/?trackingNo=TRACKINGCODE&hvtrackNoHeader=&hvfromheader=0
-//set_time_limit(0);
+/*  Poslaju Tracking API created by Afif Zafri.
+    Tracking details are fetched directly from Poslaju tracking website,
+    parse the content, and return JSON formatted string.
+    Please note that this is not the official API, this is actually just a "hack",
+    or workaround for implementing Poslaju tracking feature in other project.
+    Usage: http://site.com/index.php?trackingNo
+*/
 
-$trackingNo = '';
+if(isset($_GET['trackingNo']))
+{
+    $trackingNo = $_GET['trackingNo']; # put your poslaju tracking number here 
 
-# store post data into array
-$postdata = http_build_query(
-    array(
-        'trackingNo' => $trackingNo,
-        'hvtrackNoHeader' => '',
-        'hvfromheader' => 0
-    )
-);
+    $url = "http://poslaju.com.my/track-trace-v2/"; # url of poslaju tracking website
 
-# create option array for steam
-$opts = array('http' =>
-    array(
-        'method'  => 'POST',
-        'header'  => 'Content-type: application/x-www-form-urlencoded',
-        'content' => $postdata
-    )
-);
+    # store post data into array (poslaju website only receive the tracking no with POST, not GET. So we need to POST data)
+    $postdata = http_build_query(
+        array(
+            'trackingNo' => $trackingNo,
+            'hvtrackNoHeader' => '',
+            'hvfromheader' => 0
+        )
+    );
 
-# create new stream
-$context  = stream_context_create($opts);
+    # use cURL instead of file_get_contents(), this is because on some server, file_get_contents() cannot be used
+    # cURL also have more options and customizable
+    $ch = curl_init(); # initialize curl object
+    curl_setopt($ch, CURLOPT_URL, $url); # set url
+    curl_setopt($ch, CURLOPT_POST, 1); # set option for POST data
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata); # set post data array
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); # receive server response
+    $result = curl_exec($ch); # execute curl, fetch webpage content
+    curl_close($ch);  # close curl
 
-# fetch webpage content with post data
-$result = file_get_contents('http://poslaju.com.my/track-trace-v2/', false, $context);
+    print_r($result);
 
-print_r($result);
+}
 
 
 ?>
