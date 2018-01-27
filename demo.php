5056b5151d0f76ef804aa604d5d5103faefc0f93
Afif Zafri <zafri_21596@yahoo.com>
2018-01-27 23:32:52 +0800

    added form input for demo page

--- demo.php
+++ demo.php
@@ -5,46 +5,61 @@
 </head>
 <body>
 
+	<form action="./demo.php" method="get">
+		Tracking No: &nbsp; &nbsp; 
+		<input type="text" name="trackingNo" placeholder="Tracking no, ex: ER126406955MY" value="ER126406955MY"> &nbsp; &nbsp;
+		<button type="submit">Track</button>
+	</form>
+
 	<?php
 
-	$trackingNo = "ER126406955MY"; # your tracking number
-	$url = "http://localhost/poslajuAPI/api.php?trackingNo=".$trackingNo; # the full URL to the API
-	$getdata = file_get_contents($url); # use files_get_contents() to fetch the data, but you can also use cURL, or javascript/jquery json
-	$parsed = json_decode($getdata,true); # decode the json into array. set true to return array instead of object
-
-	$httpcode = $parsed["http_code"];
-	$message = $parsed["message"];
-
-	echo $message . "<br>";
-
-	if($message == "Record Found" && $httpcode == 200)
+	if(isset($_GET['trackingNo']))
 	{
-		?>
 
-		<table border=1>
-			<tr>
-				<th>Date/Time</th>
-				<th>Process</th>
-				<th>Event</th>
-			</tr>
+		$trackingNo = $_GET['trackingNo']; # your tracking number
+		$url = "http://localhost/poslajuAPI/api.php?trackingNo=".$trackingNo; # the full URL to the API
+		$getdata = file_get_contents($url); # use files_get_contents() to fetch the data, but you can also use cURL, or javascript/jquery json
+		$parsed = json_decode($getdata,true); # decode the json into array. set true to return array instead of object
+
+		$httpcode = $parsed["http_code"];
+		$message = $parsed["message"];
+
+		echo "<br>Status: ".$message . "<br>";
+
+		if($message == "Record Found" && $httpcode == 200)
+		{
+			?>
+
+			<table border=1>
+				<tr>
+					<th>Date/Time</th>
+					<th>Process</th>
+					<th>Event</th>
+				</tr>
+				<?php
+					
+					# iterate through the array
+					for($i=0;$i<count($parsed['data']);$i++) 
+					{
+						# access each items in the JSON
+						echo "
+							<tr>
+								<td>".$parsed['data'][$i]['date_time']."</td>
+								<td>".$parsed['data'][$i]['process']."</td>
+								<td>".$parsed['data'][$i]['event']."</td>
+							</tr>
+							";
+					}
+			?>
+
+			</table>
+
 			<?php
-				
-				# iterate through the array
-				for($i=0;$i<count($parsed['data']);$i++) 
-				{
-					# access each items in the JSON
-					echo "
-						<tr>
-							<td>".$parsed['data'][$i]['date_time']."</td>
-							<td>".$parsed['data'][$i]['process']."</td>
-							<td>".$parsed['data'][$i]['event']."</td>
-						</tr>
-						";
-				}
-	}
-	?>
+		}
 
-	</table>
+	}
+	
+	?>
 
 </body>
 </html>
