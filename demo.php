60a70a30ab740494fb8d52c07aca48ec5726e8e3
Afif Zafri <afif@redant.my>
2019-12-18 17:33:19 +0800

    automatically get the current full url domain

--- demo.php
+++ demo.php
@@ -6,7 +6,7 @@
 <body>
 
 	<form action="./demo.php" method="get">
-		Tracking No: &nbsp; &nbsp; 
+		Tracking No: &nbsp; &nbsp;
 		<input type="text" name="trackingNo" placeholder="Tracking no, ex: ER126406955MY" value="ER126406955MY"> &nbsp; &nbsp;
 		<button type="submit">Track</button>
 	</form>
@@ -17,7 +17,7 @@
 	{
 
 		$trackingNo = $_GET['trackingNo']; # your tracking number
-		$url = "http://localhost/poslajuAPI/api.php?trackingNo=".$trackingNo; # the full URL to the API
+		$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://".$_SERVER['HTTP_HOST']."/api.php?trackingNo=".$trackingNo; # the full URL to the API
 		$getdata = file_get_contents($url); # use files_get_contents() to fetch the data, but you can also use cURL, or javascript/jquery json
 		$parsed = json_decode($getdata,true); # decode the json into array. set true to return array instead of object
 
@@ -37,9 +37,9 @@
 					<th>Event</th>
 				</tr>
 				<?php
-					
+
 					# iterate through the array
-					for($i=0;$i<count($parsed['data']);$i++) 
+					for($i=0;$i<count($parsed['data']);$i++)
 					{
 						# access each items in the JSON
 						echo "
@@ -68,7 +68,7 @@
 		}
 
 	}
-	
+
 	?>
 
 </body>
