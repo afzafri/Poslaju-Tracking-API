cedd506acc8c756ba9de9cfeb7cf3c18423a4e82
Afif Zafri <afif@redant.my>
2025-04-22 11:38:48 +0800

    Use latest Poslaju endpoint to get the tracking info

--- src/PoslajuTrackingApi.php
+++ src/PoslajuTrackingApi.php
@@ -6,77 +6,87 @@ class PoslajuTrackingApi
 {
     public static function crawl($trackingNo, $include_info = false)
     {
-		$url = "https://sendparcel.poslaju.com.my/open/trace?tno={$trackingNo}";
+		$domain = "ttu-svc.pos.com.my";
+        $url = "https://$domain/api/trackandtrace/v1/request";
+        $postData = json_encode([
+            "connote_ids" => [trim($trackingNo)],
+            "culture" => "en"
+        ]);
 
-	    $ch = curl_init();
-	    curl_setopt($ch, CURLOPT_URL, $url);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	    curl_setopt($ch, CURLOPT_TIMEOUT, 5); //timeout in seconds
-	    $result = curl_exec($ch);
-	    $httpstatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	    $errormsg = (curl_error($ch)) ? curl_error($ch) : "No error";
-	    curl_close($ch);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10); // timeout in seconds
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        
+        // Set headers
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'P-Request-ID: ' . self::generateUUID(),
+            'Accept: application/json, text/plain, */*',
+            'Content-Type: application/json',
+            'host: ' . $domain
+        ]);
+        
+        $result = curl_exec($ch);
+        $httpstatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $errormsg = (curl_error($ch)) ? curl_error($ch) : "No error";
+        curl_close($ch);
 
-	    # using regex (regular expression) to parse the HTML webpage.
-	    # we only want to good stuff
-	    # regex patern
-	    $patern = "/<table.*?>(.*?)<\/table>/si";
-	    # execute regex
-	    preg_match_all($patern, $result, $parsed);
-	    // echo "<table>".$parsed[1][0]."</table>";
-	    # parse the table by row <tr>
-	    $trpatern = "#<tr>(.*?)</tr>#";
-	    preg_match_all($trpatern, implode('', $parsed[0]), $tr);
+        // Initialize response array
+        $trackres = array();
+        $trackres['http_code'] = $httpstatus;
+        $trackres['error_msg'] = $errormsg;
 
-	    // unset($tr[0][0]); # remove an array element because we don't need the 1st row (<th></th>)
-	    // unset($tr[0][1]);
-	    $tr[0] = array_values($tr[0]); # rearrange the array index
+        // Parse JSON response
+        $jsonData = json_decode($result, true);
+        
+        // Check if response is valid and has tracking data
+        if ($httpstatus == 200 && !empty($jsonData) && isset($jsonData['data']) && !empty($jsonData['data'])) {
+            $trackingData = $jsonData['data'][0] ?? null;
+            
+            if ($trackingData && isset($trackingData['tracking_data']) && !empty($trackingData['tracking_data'])) {
+                $trackres['status'] = 1;
+                $trackres['message'] = "Record Found";
 
-	    # array for keeping the data
-	    $trackres = array();
-	    $trackres['http_code'] = $httpstatus; # set http response code into the array
-	    $trackres['error_msg'] = $errormsg; # set error message into array
+                // Process tracking events
+                $trackres['data'] = [];
+                foreach ($trackingData['tracking_data'] as $i => $event) {
+                    $trackres['data'][$i] = [
+                        'date_time' => isset($event['date']) ? $event['date'] : '',
+                        'process' => isset($event['process']) ? $event['process'] : '',
+                        'event' => isset($event['process_summary']) ? $event['process_summary'] : '',
+                    ];
+                }
+            } else {
+                $trackres['status'] = 0;
+                $trackres['message'] = "No Record Found";
+            }
+        } else {
+            $trackres['status'] = 0;
+            $trackres['message'] = "No Record Found or API Error";
+        }
 
-	    # checking if record found or not, by checking the number of rows available in the result table
-	    if(count($tr[0]) > 0)
-	    {
-	    	$trackres['status'] = 1;
-	        $trackres['message'] = "Record Found"; # return record found if number of row > 0
+        if ($include_info) {
+            // Project info
+            $trackres['info']['creator'] = "Afif Zafri (afzafri)";
+            $trackres['info']['project_page'] = "https://github.com/afzafri/Poslaju-Tracking-API";
+            $trackres['info']['date_updated'] = "22/04/2025"; // Updated to current date in DD/MM/YYYY format
+        }
 
-	        # record found, so proceed
-	        # iterate through the array, access the data needed and store into new array
-	        for($i=0;$i<count($tr[0]);$i++)
-	        {
-	            # parse the table by column <td>
-	            $tdpatern = "#<td>(.*?)</td>#";
-	            preg_match_all($tdpatern, $tr[0][$i], $td);
+        return $trackres;
+    }
 
-	            # store into variable, strip_tags is for removeing html tags
-	            $datetime = strip_tags($td[0][0]);
-	            $process = strip_tags($td[0][1]);
-	            $event = strip_tags($td[0][2]);
-
-	            # store into associative array
-	            $trackres['data'][$i]['date_time'] = $datetime;
-	            $trackres['data'][$i]['process'] = $process;
-	            $trackres['data'][$i]['event'] = $event;
-	        }
-	    } else {
-	        $trackres['message'] = "No Record Found"; # return record not found if number of row < 0
-	        # since no record found, no need to parse the html furthermore
-	        $trackres['status'] = 0;
-	    }
-
-	    if ($include_info) {
-		    # project info, move it here so people see the good stuff first
-		    $trackres['info']['creator'] = "Afif Zafri (afzafri)";
-		    $trackres['info']['project_page'] = "https://github.com/afzafri/Poslaju-Tracking-API";
-		    $trackres['info']['date_updated'] = "11/11/2020";
-		}
-
-	    return $trackres;
-	
+    private static function generateUUID()
+    {
+        // Format: xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
+        // where y is replaced with (3 & random_value | 8) to ensure variant 1
+        $data = random_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // Version 4
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // Variant 1
+        
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 }
